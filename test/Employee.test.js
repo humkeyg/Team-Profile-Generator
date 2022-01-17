5294c70e273b506f3e5769da949ddfba7d6e6830
humkeyg <87458845+humkeyg@users.noreply.github.com>
2022-01-16 19:15:56 -0800

    updated employee.test.js file

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,25 +1,5 @@
 const Employee = require("../lib/Employee");
 
-test("Does this return a new class object", () => {
-    const emp = new Employee();
-    expect(typeof emp).toBe("object");
-});
-
-test("Does this set a name in the new class", () => {
-    const emp = new Employee("Frog Lady");
-    expect(emp.name).toBe("Frog Lady");
-});
-
-test("Does this set an ID in the new class", () => {
-    const emp = new Employee("Frog Lady", 2);
-    expect(emp.id).toBe(100);
-});
-
-test("Does this set an Email in the new class", () => {
-    const emp = new Employee("Frog Lady", 2, "froglady123@gmail.com");
-    expect(emp.email).toBe("froglady123@gmail.com");
-});
-
 test("Does this function return a name", () => {
     const emp = new Employee("Frog Lady", 2, "froglady123@gmail.com");
     expect(emp.getName()).toBe("Frog Lady");
